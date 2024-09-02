61e8fc585820c83f7dbada2f5ec093590b7d7450
sh20raj <shaswatraj3@gmail.com>
2024-09-02 14:48:37 +0530

    disqus

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -6,6 +6,24 @@ import {
 } from "@/components/ui/accordion";
 
 export function FAQ() {
+
+  let faqs = [
+    {
+      question: "Is it accessible?",
+      answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+    },
+    {
+      question: "Is it styled?",
+      answer:
+        "Yes. It comes with default styles that matches the other components' aesthetic.",
+    },
+    {
+      question: "Is it animated?",
+      answer:
+        "Yes. It's animated by default, but you can disable it if you prefer.",
+    },
+  ];
+  
   return (
     <>
       <div className=" p-2">
@@ -16,13 +34,13 @@ export function FAQ() {
 
         <Accordion type="single" collapsible className="w-full">
           <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionTrigger>How to add content?</AccordionTrigger>
             <AccordionContent>
               Yes. It adheres to the WAI-ARIA design pattern.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionTrigger>How to contact developer?</AccordionTrigger>
             <AccordionContent>
               Yes. It comes with default styles that matches the other
               components&apos; aesthetic.
--- src/components/PopComments.js
+++ src/components/PopComments.js
@@ -1,5 +1,4 @@
-
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Drawer,
   DrawerClose,
@@ -9,26 +8,45 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "@/components/ui/drawer"
-
+} from "@/components/ui/drawer";
 
 
 export function PopComments() {
-
-
   return (
     <Drawer>
       <DrawerTrigger asChild>
-        <Button variant="outline">Open Comments  {">"}</Button>
+        <Button variant="outline">Open Comments {">"}</Button>
       </DrawerTrigger>
       <DrawerContent>
         <div className="mx-auto w-full max-w-sm">
           <DrawerHeader>
             <DrawerTitle>Student Comments</DrawerTitle>
-            <DrawerDescription>Share your thoughts related to studies*.</DrawerDescription>
+            <DrawerDescription>
+              Share your thoughts related to studies*.
+            </DrawerDescription>
           </DrawerHeader>
+        </div>
+        <div>
+
+<div id="disqus_thread" />
+<script dangerouslySetInnerHTML={{ __html : `
+ var disqus_config = function () {
+    this.page.url = "PAGE_URL"; 
+    this.page.identifier = "PAGE_IDENTIFIER";
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://bitsyll.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+`}}>
+   
+</script>
+          
         </div>
       </DrawerContent>
     </Drawer>
-  )
+  );
 }
