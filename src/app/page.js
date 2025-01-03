3fe5bac942fb0d4124ce1f924e29abe56e0a93e5
sh20raj <shaswatraj3@gmail.com>
2025-01-03 18:03:04 +0530

    Added Download App Button

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,29 @@
-
-
 import { FAQ } from "@/components/FAQ";
 import Header from "@/components/Header";
 import ImportantData from "@/components/ImportantData";
 import { Subjects } from "@/components/Subjects2";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       <Header />
 
-
       <Subjects />
 
       <ImportantData />
 
       <FAQ />
+
+      <div className="flex justify-center my-8">
+        <a
+          href="https://median.co/share/ayewex"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="link">Download App</Button>
+        </a>
+      </div>
     </main>
   );
 }
