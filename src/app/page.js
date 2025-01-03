3911032c45fceb1c4ace6d7ac98e0a927e2fa494
sh20raj <shaswatraj3@gmail.com>
2025-01-03 18:14:02 +0530

    Enhance Download App button with icons for Android and Apple

--- src/app/page.js
+++ src/app/page.js
@@ -21,9 +21,16 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Button variant="link">Download App</Button>
+          <Button className=" border border-white" variant="link">
+            <img className="h-6 w-6 mr-2" src="/icons/android.svg" /> Download
+            App <img className="h-6 w-6 ml-2" src="/icons/apple.svg" />{" "}
+          </Button>
         </a>
       </div>
+
+
+
+      
     </main>
   );
 }
