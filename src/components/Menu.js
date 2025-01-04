7bbe9b6437bb818d366704c489bf54d90177e971
sh20raj <shaswatraj3@gmail.com>
2025-01-04 17:41:52 +0530

    new logo

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -82,8 +82,8 @@ const DATA = {
 export  default function DockDemo() {
   return (
     <div className="relative flex h-[300px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        Bit<span className="text-fuchsia-500">Syll</span>
+      <span className="pointer-events-none whitespace-pre-wrap text-center text-8xl font-bold leading-none">
+        <span className="text-white">Bit</span><span className="bg-fuchsia-500  rounded-3xl text-black ml-2 p-2">Syll</span>
       </span>
         <Menu/>
     </div>
